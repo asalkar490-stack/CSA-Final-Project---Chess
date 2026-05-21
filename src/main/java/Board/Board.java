8f81ff2e197140281282d6a65636e05c4d4a3c19
squ420-lgtm <squ420@student.fuhsd.org>
2026-05-21 12:26:00 -0700

    racism is over
    You can now play as black!!!

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -14,10 +14,10 @@ public class Board {
             }
         } else if (input.equals("playaswhite")) {
             for (int i = 0; i < board.length; i++) {
-                board[6][i] = new Pawn("Black", 6, i);
+                board[6][i] = new Pawn("White", 6, i);
             }
             for (int i = 0; i < board.length; i++) {
-                board[1][i] = new Pawn("White", 6, i);
+                board[1][i] = new Pawn("Black", 6, i);
             }
             board[7][0] = new Rook("White", 7, 0);
             board[7][1] = new Knight("White", 7, 1);
@@ -36,6 +36,31 @@ public class Board {
             board[0][5] = new Bishop("Black", 0, 5);
             board[0][6] = new Knight("Black", 0, 6);
             board[0][7] = new Rook("Black", 0, 7);
+
+        } else if (input.equals("playasblack")) {
+            for (int i = 0; i < board.length; i++) {
+                board[6][i] = new Pawn("Black", 6, i);
+            }
+            for (int i = 0; i < board.length; i++) {
+                board[1][i] = new Pawn("White", 6, i);
+            }
+            board[7][0] = new Rook("Black", 7, 0);
+            board[7][1] = new Knight("Black", 7, 1);
+            board[7][2] = new Bishop("Black", 7, 2);
+            board[7][3] = new King("Black", 7, 3);
+            board[7][4] = new Queen("Black", 7, 4);
+            board[7][5] = new Bishop("Black", 7, 5);
+            board[7][6] = new Knight("Black", 7, 6);
+            board[7][7] = new Rook("Black", 7, 7);
+
+            board[0][0] = new Rook("White", 0, 0);
+            board[0][1] = new Knight("White", 0, 1);
+            board[0][2] = new Bishop("White", 0, 2);
+            board[0][3] = new King("White", 0, 3);
+            board[0][4] = new Queen("White", 0, 4);
+            board[0][5] = new Bishop("White", 0, 5);
+            board[0][6] = new Knight("White", 0, 6);
+            board[0][7] = new Rook("White", 0, 7);
         }
     }
 
