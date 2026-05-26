5bd6df2973d78ba2134669c619a5701062d7d8e3
praxium <Pranavacoolkid@gmail.com>
2026-05-25 22:24:28 -0700

    Bug Fixes

--- src/main/java/Pieces/King.java
+++ src/main/java/Pieces/King.java
@@ -29,18 +29,26 @@ public class King extends Piece {
     /**
      * Determines whether moving this King to the specified position is a legal move.
      * A move is legal if the destination is within one square in any direction
-     * from the King's current position.
+     * from the King's current position, or if castling to that position is legal.
+     * Does not check if the destination square is under attack (that is handled
+     * by the move simulation in hasNoLegal / computeValidMoves).
      * @param row   the target row to move to
      * @param col   the target column to move to
      * @param board the current state of the chess board, where {null} represents an empty square
-     * @return {@code true} if the move is within one square, {alse} otherwise
+     * @return {@code true} if the move is within one square or is a valid castle, {false} otherwise
      */
     public boolean isLegal(int row, int col, Piece[][] board){
 
         if (row == this.row && col == this.col)
             return false;
 
+        if (board[row][col] != null && board[row][col].getColor().equals(this.getColor()))
+            return false;
+
         if (Math.abs(row - this.row) <= 1 && Math.abs(col - this.col) <= 1)
+            return true;
+
+        if (canCastle(row, col, board))
             return true;
 
         return false;
@@ -63,33 +71,37 @@ public class King extends Piece {
                 Piece temp = board[row][0];
                 board[row][0] = null;
                 board[row][3] = temp;
+                if (temp != null) { temp.setRow(row); temp.setCol(3); temp.moved(); }
             }
 
             //black kingside
-            if (row == 0 && col == 6){
+            else if (row == 0 && col == 6){
                 board[this.row][this.col] = null;
                 board[row][col] = this;
                 Piece temp = board[row][7];
                 board[row][7] = null;
                 board[row][5] = temp;
+                if (temp != null) { temp.setRow(row); temp.setCol(5); temp.moved(); }
             }
 
             //white, queenside
-            if (row == 7 && col == 2){
+            else if (row == 7 && col == 2){
                 board[this.row][this.col] = null;
                 board[row][col] = this;
                 Piece temp = board[row][0];
                 board[row][0] = null;
                 board[row][3] = temp;
+                if (temp != null) { temp.setRow(row); temp.setCol(3); temp.moved(); }
             }
 
             //white kingside
-            if (row == 7 && col == 6){
+            else if (row == 7 && col == 6){
                 board[this.row][this.col] = null;
                 board[row][col] = this;
                 Piece temp = board[row][7];
                 board[row][7] = null;
                 board[row][5] = temp;
+                if (temp != null) { temp.setRow(row); temp.setCol(5); temp.moved(); }
             }
 
             this.row = row;
@@ -107,39 +119,92 @@ public class King extends Piece {
         return hasMoved;   
     }
 
+    @Override
+    public void moved() {
+        hasMoved = true;
+    }
+
     
     /**
      * Determines whether this King is eligible to castle to the specified position.
+     * Also verifies that the King does not pass through or land on a threatened square.
      * @param row   the target row of the castling destination
      * @param col   the target column of the castling destination
      * @param board the current state of the chess board
      * @return {true} if castling to the given position is legal, {false} otherwise
      */
     public boolean canCastle(int row, int col, Piece[][] board){
-        if (hasMoved())return false;
+        if (hasMoved()) return false;
+
+        String opponentColor = this.getColor().equals("White") ? "Black" : "White";
+
+        // King cannot castle while in check
+        for (int ar = 0; ar < board.length; ar++){
+            for (int ac = 0; ac < board[0].length; ac++){
+                if (board[ar][ac] != null && board[ar][ac].getColor().equals(opponentColor) && board[ar][ac].isLegal(this.row, this.col, board))
+                    return false;
+            }
+        }
 
         //check white side
         if (this.getColor().equals("White")){
 
             //white queenside
-            if (row == 7 && col == 2 && board[7][0] != null && board[7][0].getType().equals("Rook") && !board[7][0].hasMoved() && board[7][1] == null && board[7][2] == null && board[7][3] == null)
+            if (row == 7 && col == 2 && board[7][0] != null && board[7][0].getType().equals("Rook") && !board[7][0].hasMoved() && board[7][1] == null && board[7][2] == null && board[7][3] == null){
+                // Check king doesn't pass through check on col 3 or col 2
+                for (int passCol = 2; passCol <= 3; passCol++){
+                    for (int ar = 0; ar < board.length; ar++){
+                        for (int ac = 0; ac < board[0].length; ac++){
+                            if (board[ar][ac] != null && board[ar][ac].getColor().equals(opponentColor) && board[ar][ac].isLegal(7, passCol, board))
+                                return false;
+                        }
+                    }
+                }
                 return true;
+            }
 
             //white kingside
-            if (row == 7 && col == 6 && board[7][7] != null && board[7][7].getType().equals("Rook") && !board[7][7].hasMoved() && board[7][5] == null && board[7][6] == null)
+            if (row == 7 && col == 6 && board[7][7] != null && board[7][7].getType().equals("Rook") && !board[7][7].hasMoved() && board[7][5] == null && board[7][6] == null){
+                for (int passCol = 5; passCol <= 6; passCol++){
+                    for (int ar = 0; ar < board.length; ar++){
+                        for (int ac = 0; ac < board[0].length; ac++){
+                            if (board[ar][ac] != null && board[ar][ac].getColor().equals(opponentColor) && board[ar][ac].isLegal(7, passCol, board))
+                                return false;
+                        }
+                    }
+                }
                 return true;
+            }
         }
 
-        //check left side
+        //check black side
         if (this.getColor().equals("Black")){
 
             //black queenside
-            if (row == 0 && col == 2 && board[0][0] != null && board[0][0].getType().equals("Rook") && !board[0][0].hasMoved() && board[0][1] == null && board[0][2] == null && board[0][3] == null)
+            if (row == 0 && col == 2 && board[0][0] != null && board[0][0].getType().equals("Rook") && !board[0][0].hasMoved() && board[0][1] == null && board[0][2] == null && board[0][3] == null){
+                for (int passCol = 2; passCol <= 3; passCol++){
+                    for (int ar = 0; ar < board.length; ar++){
+                        for (int ac = 0; ac < board[0].length; ac++){
+                            if (board[ar][ac] != null && board[ar][ac].getColor().equals(opponentColor) && board[ar][ac].isLegal(0, passCol, board))
+                                return false;
+                        }
+                    }
+                }
                 return true;
+            }
 
             //black kingside
-            if (row == 0 && col == 6 && board[0][7] != null && board[0][7].getType().equals("Rook") && !board[0][7].hasMoved() && board[0][5] == null && board[0][6] == null)
+            if (row == 0 && col == 6 && board[0][7] != null && board[0][7].getType().equals("Rook") && !board[0][7].hasMoved() && board[0][5] == null && board[0][6] == null){
+                for (int passCol = 5; passCol <= 6; passCol++){
+                    for (int ar = 0; ar < board.length; ar++){
+                        for (int ac = 0; ac < board[0].length; ac++){
+                            if (board[ar][ac] != null && board[ar][ac].getColor().equals(opponentColor) && board[ar][ac].isLegal(0, passCol, board))
+                                return false;
+                        }
+                    }
+                }
                 return true;
+            }
         }
         return false;
     }
